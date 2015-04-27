25ced80e447e83c5d653e3cb83070cbfc896fbb3
b4tm4n <josegarvinvictoria@gmail.com>
2015-04-27 13:31:43 +0200

    Connexió amb la base de dades correcte

--- src/main/java/net/josegarvin/servlets/test.java
+++ src/main/java/net/josegarvin/servlets/test.java
@@ -2,7 +2,6 @@ package net.josegarvin.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 import javax.annotation.Resource;
 import javax.persistence.EntityManager;
@@ -21,49 +20,46 @@ import javax.transaction.UserTransaction;
 
 import net.josegarvin.model.Planetes;
 
-
 /**
- * Servlet implementation class Servlet1
+ * Servlet implementation class test
  */
-
-@WebServlet("/Servlet1")
-public class Servlet1 extends HttpServlet {
+@WebServlet("/test")
+public class test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Servlet1() {
+    public test() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-
+    
     @PersistenceContext(unitName="Planets-persistence-unit") 
     EntityManager em; 
 
     @Resource
     UserTransaction tx;
-    
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
-//		mart.setDiametre(2000.0);
-//		mart.setDistancia(2000.0);
-//		mart.setVelocitat(2000.0);
+		
+
 		
 		try {
 			tx.begin();
-			Planetes planeta = new Planetes();
-		    planeta.setNom("Pluto");
-		    planeta.setDiametre(100);    
-		    
+			Planetes pluto = new Planetes();
 			
-			em.persist(planeta);
+			pluto.setNom("Pluto");
+			pluto.setDiametre(2000.0);
+			pluto.setDistancia(2000.0);
+			pluto.setVelocitat(2000.0);
 			
+			em.persist(pluto);
 			em.flush();
 			tx.commit();
 		} catch (SecurityException | IllegalStateException | RollbackException
@@ -79,13 +75,6 @@ public class Servlet1 extends HttpServlet {
 		
 		out.println("Mart afegit!");
 	}
-	
-	  private Planetes crearPlaneta(String nom) {
-		    Planetes planeta = new Planetes();
-		    planeta.setNom(nom);
-		    planeta.setDiametre(100);    
-		    return planeta;
-		  }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
